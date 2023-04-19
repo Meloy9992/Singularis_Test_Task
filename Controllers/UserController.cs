bf904c955a8604e602133d303d0539d72f373851
Perzival9992 <noir99@yandex.ru>
2023-04-19 23:38:56 +0300

    Создан приватный список со временными данными Создан Get запрос с получением всего списка данных

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,10 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Singularis_Test_Task.Models;
 
 namespace Singularis_Test_Task.Controllers
 {
     [Route("[controller]")]
     public class UserController : Controller
     {
+        private static List<User> users = new List<User>(new[]
+        {
+            new User() {firstName = "Игорь", lastName = "Иванов",
+                address = "Москва, Ленинский проспект", dateBirthday = "20 августа 1900",
+            email = "qwerty1@yandex.ru", phoneNumber = "123456789"},
+
+            new User() {firstName = "Евгений", lastName = "Иванка",
+                address = "Москва, Ленинский проспектик", dateBirthday = "21 августа 1900",
+            email = "qwerty2@yandex.ru", phoneNumber = "123456789"},
+
+            new User() {firstName = "Анатолий", lastName = "Ивановичкин",
+                address = "Москва, Ленинский проспектун", dateBirthday = "22 августа 1900",
+            email = "qwerty3@yandex.ru", phoneNumber = "123456789"}
+        });
+
+
+
+        [HttpGet]
+        public IEnumerable<User> Get() => users;
+        
+
         public IActionResult Index()
         {
             return View();
